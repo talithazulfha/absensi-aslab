fdf079c22adc18b1b862f3994cc95b599c4a3999
talithazulfha <zh.talitha13@gmail.com>
2024-05-20 13:19:02 +0700

    preline2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./views/*.{html,js,ejs}",
     "./views/aslab/*.{html,js,ejs}",
+    "./views/admin/*.{html,js,ejs}",
     "node_modules/preline/dist/*.js,"
   ],
   theme: {
